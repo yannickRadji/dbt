6b0d3e9d900967b56d66e48a73c4207e3992413f
yannick.radji.ext@efs.sante.fr <yannick.radji.ext@efs.sante.fr>
2024-12-31 18:35:05 +0000

    avoid duplicated patient, analysis & delivrance initially it was only union

--- models/convergence_analyses.sql
+++ models/convergence_analyses.sql
@@ -1,18 +1,35 @@
-WITH region1 AS (
-    SELECT * FROM {{ ref('region1_raw_analyses') }}
+WITH deduplicated_patients AS (
+    SELECT DISTINCT
+        COALESCE(r1.patient_id, r2.patient_id) AS patient_id,
+        COALESCE(r1.first_name, r2.first_name) AS first_name,
+        COALESCE(r1.last_name, r2.last_name) AS last_name,
+        COALESCE(r1.birth_date, r2.birth_date) AS birth_date
+    FROM region1_raw.patients r1
+    FULL OUTER JOIN region2_raw.patients r2
+        ON r1.first_name = r2.first_name
+        AND r1.last_name = r2.last_name
+        AND r1.birth_date = r2.birth_date
 ),
-region2 AS (
-    SELECT * FROM {{ ref('region2_raw_analyses') }}
+region1_analyses AS (
+    SELECT
+        analysis_id AS analysis_id, 
+        patient_id,
+        analysis_date,
+        blood_group,
+        status
+    FROM region1_raw.analyses
 ),
-combined AS (
-    SELECT * FROM region1
-    UNION ALL
-    SELECT * FROM region2
+region2_analyses AS (
+    SELECT
+        analysis_id + 1000000 AS analysis_id, -- Ensure the offset is large enough to avoid overlap with existing IDs in Region without changing type
+        patient_id,
+        analysis_date,
+        blood_group,
+        status
+    FROM region2_raw.analyses
 )
-SELECT
-    analysis_id,
-    patient_id,
-    analysis_date,
-    blood_group,
-    status
-FROM combined
+SELECT *
+FROM region1_analyses
+UNION ALL
+SELECT *
+FROM region2_analyses;
--- models/convergence_delivrances.sql
+++ models/convergence_delivrances.sql
@@ -1,18 +1,35 @@
-WITH region1 AS (
-    SELECT * FROM {{ ref('region1_raw_delivrances') }}
+WITH deduplicated_patients AS (
+    SELECT DISTINCT
+        COALESCE(r1.patient_id, r2.patient_id) AS patient_id,
+        COALESCE(r1.first_name, r2.first_name) AS first_name,
+        COALESCE(r1.last_name, r2.last_name) AS last_name,
+        COALESCE(r1.birth_date, r2.birth_date) AS birth_date
+    FROM region1_raw.patients r1
+    FULL OUTER JOIN region2_raw.patients r2
+        ON r1.first_name = r2.first_name
+        AND r1.last_name = r2.last_name
+        AND r1.birth_date = r2.birth_date
 ),
-region2 AS (
-    SELECT * FROM {{ ref('region2_raw_delivrances') }}
+region1_deliverances AS (
+    SELECT
+        delivrance_id AS delivrance_id,
+        patient_id,
+        delivrance_date,
+        blood_type,
+        volume_ml
+    FROM region1_raw.deliverances
 ),
-combined AS (
-    SELECT * FROM region1
-    UNION ALL
-    SELECT * FROM region2
+region2_deliverances AS (
+    SELECT
+        delivrance_id + 1000000 AS delivrance_id,
+        patient_id,
+        delivrance_date,
+        blood_type,
+        volume_ml
+    FROM region2_raw.deliverances
 )
-SELECT
-    delivrance_id,
-    patient_id,
-    delivrance_date,
-    blood_type,
-    volume_ml
-FROM combined
+SELECT *
+FROM region1_deliverances
+UNION ALL
+SELECT *
+FROM region2_deliverances;
