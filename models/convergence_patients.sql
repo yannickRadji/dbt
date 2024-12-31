3fb3cb843e0d8acba19a1e37f31a67f2c98cf5a5
yannick.radji.ext@efs.sante.fr <yannick.radji.ext@efs.sante.fr>
2024-12-31 19:57:44 +0000

    use created table whereas the source via ref, change materialization for view for intermediate simple changes, fix dudeplication (il y avait des doubles) with patient in convergence anaylses & delivrances tables

--- models/convergence_patients.sql
+++ models/convergence_patients.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 WITH combined_patients AS (
     SELECT
         patient_id,
@@ -6,27 +8,27 @@ WITH combined_patients AS (
         birth_date,
         region,
         start_date
-    FROM region1_raw.patients
+    FROM  {{ ref('region1_raw_patients') }}
     UNION ALL
     SELECT
+        patient_id + 1000000 AS patient_id,
+        first_name,
+        last_name,
+        birth_date,
+        region,
+        start_date
+    FROM {{ ref('region2_raw_patients') }}
+),
+deduplicated_patients AS (
+    SELECT DISTINCT ON (first_name, last_name, birth_date)
         patient_id,
         first_name,
         last_name,
         birth_date,
         region,
         start_date
-    FROM region2_raw.patients
-),
-deduplicated_patients AS (
-    SELECT DISTINCT ON (first_name, last_name, birth_date)
-        patient_id, -- Keep original patient_id
-        first_name,
-        last_name,
-        birth_date,
-        region,
-        start_date
     FROM combined_patients
-    ORDER BY first_name, last_name, birth_date, start_date ASC -- Oldest start date first
+    ORDER BY first_name, last_name, birth_date, start_date ASC -- keep Oldest start date first, first_name, last_name, birth_date to align with the DISTINCT ON clause
 )
 SELECT *
 FROM deduplicated_patients
