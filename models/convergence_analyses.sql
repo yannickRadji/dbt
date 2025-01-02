f9c5a7c35595ee0c6a4c4e52b0142032ae84be86
yannick.radji.ext@efs.sante.fr <yannick.radji.ext@efs.sante.fr>
2025-01-02 14:31:51 +0000

    lint fix

--- models/convergence_analyses.sql
+++ models/convergence_analyses.sql
@@ -7,21 +7,23 @@ WITH region1_analyses AS (
         analysis_date,
         blood_group,
         status
-    FROM {{ ref('region1_raw_analyses') }} a
-    JOIN {{ ref('convergence_patients') }} dp
+    FROM {{ ref('region1_raw_analyses') }} AS a
+    INNER JOIN {{ ref('convergence_patients') }} AS dp
         ON a.patient_id = dp.patient_id
 ),
+
 region2_analyses AS (
     SELECT
-        analysis_id + 1000000 AS analysis_id,
         dp.patient_id,
         analysis_date,
         blood_group,
-        status
-    FROM {{ ref('region2_raw_analyses') }} a
-    JOIN {{ ref('convergence_patients') }} dp
+        status,
+        analysis_id + 1000000 AS analysis_id
+    FROM {{ ref('region2_raw_analyses') }} AS a
+    INNER JOIN {{ ref('convergence_patients') }} AS dp
         ON a.patient_id = dp.patient_id
 )
+
 SELECT *
 FROM region1_analyses
 UNION ALL
