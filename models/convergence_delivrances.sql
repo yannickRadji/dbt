5b0a6edcb731d5e4c0ce221dce6435a116730687
yannickRadji <yannickRadji@users.noreply.github.com>
2025-01-25 10:44:19 +0100

    Update delivrances to keep the patient ID from the region kept for duplacates & tests
    * add tests on unicity of and relationships
    
    * avoid duplicated patient, analysis & delivrance initially it was only union
    
    * correct ypos then format volume and blood type/group
    
    * use created table whereas the source via ref, change materialization for view for intermediate simple changes, fix dudeplication (il y avait des doubles) with patient in convergence anaylses & delivrances tables
    
    * lint fix
    
    * fix id issues and segregate regional transformations
    
    * mettre seulement l'ID patient qu'on a retenue pour les personnes double régions
    
    * add data test (simple without Macro)
    
    * mapping table added
    
    * add descriptions
    
    * fix delivrance
    
    * add a mapping_blood_types table wheras switch case
    
    ---------
    
    Co-authored-by: yannick.radji.ext@efs.sante.fr <yannick.radji.ext@efs.sante.fr>

--- models/convergence_delivrances.sql
+++ models/convergence_delivrances.sql
@@ -3,29 +3,45 @@
 WITH region1_deliverances AS (
     SELECT
         delivrance_id,
-        dp.patient_id,
+        d.patient_id,
+        dp.first_name,
+        dp.last_name,
+        dp.birth_date,
         delivrance_date,
         blood_type,
         volume_ml
     FROM {{ ref('region1_raw_delivrances') }} AS d
-    INNER JOIN {{ ref('convergence_patients') }} AS dp
+    INNER JOIN {{ ref('region1_raw_patients') }} AS dp
         ON d.patient_id = dp.patient_id
 ),
 
 region2_deliverances AS (
     SELECT
         delivrance_id,
-        dp.patient_id,
+        d.patient_id,
+        dp.first_name,
+        dp.last_name,
+        dp.birth_date,
         delivrance_date,
         blood_type,
         volume_ml
     FROM {{ ref('region2_raw_delivrances') }} AS d
-    INNER JOIN {{ ref('convergence_patients') }} AS dp
+    INNER JOIN {{ ref('region2_raw_patients') }} AS dp
         ON d.patient_id = dp.patient_id
-)
+),
 
-SELECT *
+unioned_delivrances AS (SELECT *
 FROM region1_deliverances
 UNION ALL
 SELECT *
-FROM region2_deliverances
+FROM region2_deliverances)
+
+SELECT
+    delivrance_id,
+    c.patient_id,
+    delivrance_date,
+    blood_type,
+    volume_ml
+FROM unioned_delivrances AS u
+INNER JOIN {{ ref("convergence_patients")}} AS c
+    ON u.first_name = c.first_name AND u.last_name = c.last_name AND u.birth_date = c.birth_date
--- models/region2_raw_analyses.sql
+++ models/region2_raw_analyses.sql
@@ -3,15 +3,7 @@ SELECT
     patient_id + 1000000 AS patient_id,
     analysis_date,
     status,
-    CASE
-        WHEN blood_group = 'Oplus' THEN 'O+'
-        WHEN blood_group = 'Aplus' THEN 'A+'
-        WHEN blood_group = 'Bplus' THEN 'B+'
-        WHEN blood_group = 'ABplus' THEN 'AB+'
-        WHEN blood_group = 'Ominus' THEN 'O-'
-        WHEN blood_group = 'Aminus' THEN 'A-'
-        WHEN blood_group = 'Bminus' THEN 'B-'
-        WHEN blood_group = 'ABminus' THEN 'AB-'
-        ELSE blood_group
-    END AS blood_group
-FROM {{ source('region2_raw', 'analyses') }}
+    mbt.standardized_value AS blood_group
+FROM {{ source('region2_raw', 'analyses') }} as a
+LEFT JOIN {{ source('national_convergence', 'mapping_blood_types') }} mbt
+    ON a.blood_group = mbt.source_value
